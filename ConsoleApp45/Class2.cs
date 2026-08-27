8fbd7295867f27619497caad554bd39436bf9359
Mahsati S. Taghiyeva <mahsatist@code.local>
2026-08-27 20:57:27 +0400

    Initial commit

---
+++ ConsoleApp45/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    //Productdan miras alır ve Genre ozelliyi var;
+    internal class Book: Product
+    {
+        public string Genre ="";
+        //Genre dəyəri təyin olunmadan Book yaradıla bilməz
+        public Book(string genre)
+        {
+            Genre = genre;
+        }
+
+    }
+}
